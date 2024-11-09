cba86631c0395f95c9a01eb7cc66484ec517fb95
Ayon <suprovatbit@gmail.com>
2024-11-10 00:19:49 +0600

    Headers in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-  return new Response("Profile API Data");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headersList = headers();
+
+  console.log(requestHeaders.get("authorization"));
+  console.log(headersList.get("authorization"));
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
