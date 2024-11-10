59ff7d2385f0ca73da77e5e083a2a01598733e35
Ayon <suprovatbit@gmail.com>
2024-11-10 12:14:55 +0600

    Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,15 +1,22 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headersList = headers();
 
+  cookies().set("resultPerPage", "20");
+  const theme = request.cookies.get("theme");
+
   console.log(requestHeaders.get("authorization"));
   console.log(headersList.get("authorization"));
+
+  console.log(theme);
+  console.log(cookies().get("resultPerPage"));
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
